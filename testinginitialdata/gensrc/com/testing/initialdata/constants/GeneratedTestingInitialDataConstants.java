1811c849c242f9d07af99505d1b71966e37e5546
Manikanta_Vanapalli <Manikanta_Vanapalli@DESKTOP-F6202FG>
2024-02-20 12:14:57 +0530

    intialdata.

---
+++ testinginitialdata/gensrc/com/testing/initialdata/constants/GeneratedTestingInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 19-Feb-2024, 5:57:34 pm                     ---
+ * ----------------------------------------------------------------
+ */
+package com.testing.initialdata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTestingInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "testinginitialdata";
+	
+	protected GeneratedTestingInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ testinginitialdata/src/com/testing/initialdata/constants/TestingInitialDataConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.testing.initialdata.constants;
+
+/**
+ * Global class for all TestingInitialData constants.
+ */
+public final class TestingInitialDataConstants extends GeneratedTestingInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "testinginitialdata";
+
+	private TestingInitialDataConstants()
+	{
+		//empty
+	}
+}
