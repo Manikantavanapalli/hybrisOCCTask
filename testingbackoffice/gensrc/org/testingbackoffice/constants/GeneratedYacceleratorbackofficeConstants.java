c19123da89b507878af964cb33e34efcb43ad3fa
Manikanta_Vanapalli <Manikanta_Vanapalli@DESKTOP-F6202FG>
2024-02-20 12:17:07 +0530

    backoffice

---
+++ testingbackoffice/gensrc/org/testingbackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 19-Feb-2024, 5:57:34 pm                     ---
+ * ----------------------------------------------------------------
+ */
+package org.testingbackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "testingbackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
